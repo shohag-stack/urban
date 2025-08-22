44551441a32b4f56dbed2644f7b657514e9b60cf
shohag-stack <shohag4y@gmail.com>
2025-08-22 18:49:08 +0600

    added dynamic import

--- app/listings/page.tsx
+++ app/listings/page.tsx
@@ -1,10 +1,16 @@
 'use client'
 import SmoothScrollWrapper from "@/components/SmoothScrollWrapper"
 import PropertyListingsLoading from "@/components/PropertyListingsLoading"
-import PropertyListings from "@/components/PropertyListings"
 import { Suspense } from "react"
+import dynamic from "next/dynamic"
 // Main page component with Suspense boundary
 export default function Page() {
+
+    const PropertyListings = dynamic(() => import('@/components/PropertyListings'), {
+    ssr: false,
+    loading: () => <PropertyListingsLoading />
+})
+
     return (
         <SmoothScrollWrapper>
             <Suspense fallback={<PropertyListingsLoading />}>
