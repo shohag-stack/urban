63bf8240c60da3b202b3a3e908db0bb73a1ec1ae
shohag-stack <shohag4y@gmail.com>
2025-08-22 19:26:40 +0600

    added dynamic import

--- app/listings/layout.tsx
+++ app/listings/layout.tsx
@@ -1,17 +1,14 @@
 'use client'
 import React from 'react'
 import { Suspense } from 'react'
-import PropertyFilters from '@/components/PropertyFilters'
-
-
-
+import dynamic from 'next/dynamic'
 
 export default function Layout({ children }: Readonly<{ children: React.ReactNode }>) {
-  
 
+  const PropertyFilters = dynamic(()=> import('../../components/PropertyFilters'))
 
   return (
-    <Suspense>
+    <Suspense fallback>
       <PropertyFilters/>
       {children}
     </Suspense>
