5f8b3c30c8b82b33fb76a77e3c40b39ddbbcf16b
shohag-stack <shohag4y@gmail.com>
2025-08-22 19:06:32 +0600

    file path changed

--- app/listings/page.tsx
+++ app/listings/page.tsx
@@ -1,11 +1,11 @@
 'use client'
-import SmoothScrollWrapper from "@/components/SmoothScrollWrapper"
-import PropertyListingsLoading from "@/components/PropertyListingsLoading"
+import SmoothScrollWrapper from "../../components/SmoothScrollWrapper"
+import PropertyListingsLoading from "../../components/PropertyListingsLoading"
 import { Suspense } from "react"
 import dynamic from "next/dynamic"
 // Main page component with Suspense boundary
 
-const PropertyListings = dynamic(() => import('@/components/PropertyListings'), {
+const PropertyListings = dynamic(() => import('../../components/PropertyListings'), {
     ssr: false,
     loading: () => <PropertyListingsLoading />
 })
