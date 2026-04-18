bc4223a1f9f33fb8e27b17a0942eaa59f8136e40
shohag-stack <shohag4y@gmail.com>
2026-04-18 10:37:31 +0600

    sanit config modified

--- sanity/env.ts
+++ sanity/env.ts
@@ -1,8 +1,10 @@
+// sanity/env.ts
+
 export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2024-04-14";
 
 export const dataset =
-  process.env.NEXT_PUBLIC_SANITY_DATASET || null;
+  process.env.NEXT_PUBLIC_SANITY_DATASET || "";
 
 export const projectId =
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || null;
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "";
